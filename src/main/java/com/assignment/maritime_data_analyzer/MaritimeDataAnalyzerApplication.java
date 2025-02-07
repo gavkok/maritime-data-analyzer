44ec75d67f3f8e480a60363d6f8bf75f93e04531
Gavriil <gavkokko@gmail.com>
2025-02-07 19:54:45 +0200

    Initial commit

---
+++ src/main/java/com/assignment/maritime_data_analyzer/MaritimeDataAnalyzerApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.maritime_data_analyzer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaritimeDataAnalyzerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MaritimeDataAnalyzerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/assignment/maritime_data_analyzer/MaritimeDataAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.maritime_data_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MaritimeDataAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
